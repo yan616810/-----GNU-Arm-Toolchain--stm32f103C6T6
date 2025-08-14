ef1359847c33b116dc3c39a4e5f15319124445c0
YLAD-nobody <yhlad0801@gmail.com>
2025-08-14 12:46:08 +0800

    第一次使用GNU Arm Toolchain编译stm32f103C6T6固件源码;

---
+++ main.c
@@ -0,0 +1,20 @@
+#include "stm32f10x.h"
+
+int main(void)
+{
+    // 配置GPIO
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+    {
+        GPIO_InitTypeDef GPIO_InitStructure;
+        GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
+        GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+        GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+        GPIO_Init(GPIOC, &GPIO_InitStructure);
+    }
+    for(;;)
+    {
+        // 切换LED状态
+        GPIOC->ODR ^= GPIO_Pin_13;
+        for (volatile int i = 0; i < 100000; i++);
+    }
+}
